70fb28530b721f8b160b6933c46361773ead1e93
Mr-Breaker1972 <pacitommaso@gmail.com>
2020-09-17 21:16:01 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,10 @@ module "aws" {
   source  = "app.terraform.io/mrbreaker2-training/aws/provider"
   version = "3.6.0"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Gaurav"
+  }
+}
