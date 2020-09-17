38b35976fc256959935d8b250aa687fc23f91a9a
Mr-Breaker1972 <pacitommaso@gmail.com>
2020-09-17 21:06:48 +0000

    Added VPC module

--- main.tf
+++ main.tf
@@ -192,10 +192,3 @@ resource aws_key_pair "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
-
-//--------------------------------------------------------------------
-// Modules
-module "aws" {
-  source  = "app.terraform.io/mrbreaker2-training/aws/provider"
-  version = "3.6.0"
-}
