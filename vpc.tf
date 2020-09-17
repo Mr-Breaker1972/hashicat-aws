b15d1fd5c60c2a32b544bbde161bd325a3416ef7
Mr-Breaker1972 <pacitommaso@gmail.com>
2020-09-17 21:33:19 +0000

    Added VPC module network

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Gaurav"
+    Name = "Gaurav1"
   }
 }
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
